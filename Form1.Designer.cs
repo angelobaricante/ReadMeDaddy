f9f62096a3dfee7b8bb1513dc6f52097ef49a019
Mhel Jane <Mhel Jane@LAPTOP-GRNPVLMN>
2024-05-05 21:45:07 +0800

    Modified UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,84 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Read Me Daddy: AI File Assistant";
-
             this.outputTextBox = new System.Windows.Forms.TextBox();
             this.fileLabel = new System.Windows.Forms.Label();
             this.processButton = new System.Windows.Forms.Button();
             this.taskInputTextBox = new System.Windows.Forms.TextBox();
             this.updateButton = new System.Windows.Forms.Button();
             this.selectFileButton = new System.Windows.Forms.Button();
-
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
             // 
             // outputTextBox
             // 
-            this.outputTextBox.Location = new System.Drawing.Point(10, 110);
+            this.outputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.outputTextBox.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.ForeColor = System.Drawing.Color.White;
+            this.outputTextBox.Location = new System.Drawing.Point(12, 172);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.outputTextBox.Multiline = true;
             this.outputTextBox.Name = "outputTextBox";
-            this.outputTextBox.Size = new System.Drawing.Size(760, 300);
             this.outputTextBox.ReadOnly = true;
             this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-
+            this.outputTextBox.Size = new System.Drawing.Size(775, 374);
+            this.outputTextBox.TabIndex = 5;
             // 
             // fileLabel
             // 
             this.fileLabel.AutoSize = true;
-            this.fileLabel.Location = new System.Drawing.Point(10, 60);
+            this.fileLabel.ForeColor = System.Drawing.Color.White;
+            this.fileLabel.Location = new System.Drawing.Point(15, 150);
+            this.fileLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.fileLabel.Name = "fileLabel";
-            this.fileLabel.Size = new System.Drawing.Size(150, 20);
+            this.fileLabel.Size = new System.Drawing.Size(125, 20);
+            this.fileLabel.TabIndex = 4;
             this.fileLabel.Text = "No file selected";
-
             // 
             // processButton
             // 
-            this.processButton.Location = new System.Drawing.Point(520, 20);
+            this.processButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
+            this.processButton.FlatAppearance.BorderSize = 0;
+            this.processButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.processButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.processButton.ForeColor = System.Drawing.Color.White;
+            this.processButton.Location = new System.Drawing.Point(796, 346);
+            this.processButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.processButton.Name = "processButton";
-            this.processButton.Size = new System.Drawing.Size(100, 30);
+            this.processButton.Size = new System.Drawing.Size(189, 49);
+            this.processButton.TabIndex = 2;
             this.processButton.Text = "Process";
-            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.UseVisualStyleBackColor = false;
             this.processButton.Click += new System.EventHandler(this.ProcessButton_Click);
-
             // 
             // taskInputTextBox
             // 
-            this.taskInputTextBox.Location = new System.Drawing.Point(120, 20);
+            this.taskInputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.taskInputTextBox.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taskInputTextBox.ForeColor = System.Drawing.Color.White;
+            this.taskInputTextBox.Location = new System.Drawing.Point(12, 121);
+            this.taskInputTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.taskInputTextBox.Name = "taskInputTextBox";
-            this.taskInputTextBox.Size = new System.Drawing.Size(380, 26);
-
+            this.taskInputTextBox.Size = new System.Drawing.Size(775, 23);
+            this.taskInputTextBox.TabIndex = 3;
             // 
             // updateButton
             // 
-            this.updateButton.Location = new System.Drawing.Point(630, 20);
+            this.updateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
+            this.updateButton.FlatAppearance.BorderSize = 0;
+            this.updateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateButton.ForeColor = System.Drawing.Color.White;
+            this.updateButton.Location = new System.Drawing.Point(796, 494);
+            this.updateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.updateButton.Name = "updateButton";
-            this.updateButton.Size = new System.Drawing.Size(100, 30);
+            this.updateButton.Size = new System.Drawing.Size(189, 54);
+            this.updateButton.TabIndex = 1;
             this.updateButton.Text = "Update";
-            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.UseVisualStyleBackColor = false;
             this.updateButton.Click += new System.EventHandler(this.UpdateButton_Click);
-
             // 
             // selectFileButton
             // 
-            this.selectFileButton.Location = new System.Drawing.Point(10, 20);
+            this.selectFileButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
+            this.selectFileButton.FlatAppearance.BorderSize = 0;
+            this.selectFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.selectFileButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectFileButton.ForeColor = System.Drawing.Color.White;
+            this.selectFileButton.Location = new System.Drawing.Point(796, 172);
+            this.selectFileButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.selectFileButton.Name = "selectFileButton";
-            this.selectFileButton.Size = new System.Drawing.Size(100, 30);
+            this.selectFileButton.Size = new System.Drawing.Size(189, 46);
+            this.selectFileButton.TabIndex = 0;
             this.selectFileButton.Text = "Select File";
-            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.UseVisualStyleBackColor = false;
             this.selectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
-
-            // Add controls to the form
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
+            this.label1.Location = new System.Drawing.Point(242, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 48);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Read Me Daddy";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(317, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "AI File Assistant";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.selectFileButton);
             this.Controls.Add(this.updateButton);
             this.Controls.Add(this.processButton);
             this.Controls.Add(this.taskInputTextBox);
             this.Controls.Add(this.fileLabel);
             this.Controls.Add(this.outputTextBox);
-
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Read Me Daddy: AI File Assistant";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
@@ -116,5 +177,7 @@
         private System.Windows.Forms.TextBox taskInputTextBox;
         private System.Windows.Forms.Button updateButton;
         private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
